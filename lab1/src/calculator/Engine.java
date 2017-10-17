9daaf18cad595b902f198334468d4e6cad0ca448
victormotogna <vmotogna@gmail.com>
2017-10-17 09:22:44 +0300

    partial

---
+++ lab1/src/calculator/Engine.java
@@ -0,0 +1,10 @@
+package calculator;
+
+public class Engine {
+    public static void main(String args[]) {
+        System.out.println(new Operation(2, 4, "+").getResult());
+        System.out.println(new Operation(2, 4, "-").getResult());
+        System.out.println(new Operation(2, 4, "*").getResult());
+        System.out.println(new Operation(2, 4, "/").getResult());
+    }
+}
