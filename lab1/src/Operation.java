efe21d895387c4e758f090c1df1d582d2ca5fca6
victormotogna <vmotogna@gmail.com>
2017-10-17 08:15:32 +0300

    mini calculator start

---
+++ lab1/src/Operation.java
@@ -0,0 +1,14 @@
+public class Operation {
+    private int operator1;
+    private int operator2;
+    private String operation;
+    private int result;
+
+    public Operation(int operator1, int operator2, String operation) {
+        this.operator1 = operator1;
+        this.operator2 = operator2;
+        this.operation = operation;
+    }
+
+
+}
